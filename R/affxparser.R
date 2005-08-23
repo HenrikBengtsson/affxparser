a94a37211a65990c15779a80c0fdda061f257c21
James Bullard <bullard@berkeley.edu>
2005-08-23 22:07:00 +0000

    Added support for CDF and CEL files. Changed some names.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@13688 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/affxparser.R
+++ R/affxparser.R
@@ -17,22 +17,52 @@
 ##  along with this program; if not, write to the Free Software
 ##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+
+#############################################################################
 ##
-## this is the public method which creates a new
-## environment from the cdf file.
+## CEL Files
 ##
-get.cdf.env <- function(fname, complementary.logic = FALSE) {
+#############################################################################
+
+getIntensityMatrix <- function(fname) {
+  .getCelFile(fname, readHeaders = FALSE, readOutliersAndMasked = FALSE)
+}
+
+getCelFile <- function(fname) {
+  .getCelFile(fname, readHeaders = TRUE, readOutliersAndMasked = TRUE)
+}
+
+.getCelFile <- function(fname, readHeaders = FALSE,
+                        readOutliersAndMasked = FALSE) {
+  fname <- file.path(dirname(fname), basename(fname))
+
+  if (!file.exists(fname))
+    stop(paste("file:", fname, "does not exist."))
+
+  cel.file <- .Call("R_affx_get_cel_file", as.character(fname),
+                    as.integer(readHeaders), as.integer(readOutliersAndMasked))
+
+  return(cel.file)
+  
+  colnames(cel.file$intensities) <- c("x","y","intensity","stdv","pixels")
+
+  return(cel.file)
+}
+
+##############################################################################
+##
+## CDF Files
+##
+##############################################################################
+
+getCdfEnv <- function(fname, complementary.logic = FALSE) {
   pmmm <- .parse.cdffile(fname, complementary.logic)
   e <- new.env()
   multiassign(names(pmmm), value = pmmm, e)
   e
 }
 
-##
-## this method returns a list, the public method
-## returns the environment.
-##
-.parse.cdffile <- function(fname, complementary.logic = FALSE) {
+.getCdfEnv <- function(fname, complementary.logic = FALSE) {
   fname <- file.path(dirname(fname), basename(fname))
 
   if (!file.exists(fname))
@@ -44,66 +74,4 @@ get.cdf.env <- function(fname, complementary.logic = FALSE) {
     stop(paste("Error parsing:", fname))
   
   lapply(pmmm, t)
-}
-
-get.cel.file <- function(fname) {
-  .get.cel.file(fname)
-}
-
-##
-## this method returns a list of the *entire* cel file.
-## the public method is more discerning. 
-##
-.get.cel.file <- function(fname) {
-  fname <- file.path(dirname(fname), basename(fname))
-
-  if (!file.exists(fname))
-    stop(paste("file:", fname, "does not exist."))
-
-  cel.file <- .Call("R_affx_get_cel_file", as.character(fname))
-
-  colnames(cel.file$intensities) <- c("x","y","intensity","stdv","pixels")
-  return(cel.file)
-}
-
-##
-## this method constructs the affybatch object using the
-## the parsing utilities in this library.
-##
-const.affybatch <- function(cel.files, ...){
-  exprMatrix <- sapply(cel.files, function(x) x$intensities[,3])
-  nrows <- cel.files[[1]]$header$rows
-  ncols <- cel.files[[1]]$header$cols
-  cdfname <- cel.files[[1]]$header$ChipType
-  pheno <- new("phenoData", pData = data.frame(chip = seq(length = length(cel.files))),
-               varLabels = list(chip = "Chip identifier"))
-
-  abatch <- new("AffyBatch", exprs = exprMatrix, ncol = ncols,
-             nrow = nrows, phenoData = pheno, cdfName = cdfname)
-
-  return(abatch)
-}
-
-##
-## what follows are some utility methods for testing
-##
-## FIXME: Remove on release of package. 
-
-##
-## compare the results of our cdfenv to that
-## of make.cdf.env
-##
-.compare.parses <- function(fname) {
-  cdf.env <- make.cdf.env(fname)
-  print(paste(length(ls(cdf.env)), "probeset names from make.cdf.env"))
-  pmmm <- .parse.cdffile(fname)
-  print(paste(length(pmmm), "probeset names from get.cdf.env"))
-
-  for (name in ls(cdf.env)) {
-    if (any((pmmm[[name]] == get(name, cdf.env)) == FALSE)) {
-      print(paste("Disagreement in", name))
-      print(pmmm[name])
-      print(get(name, cdf.env))
-    }
-  }
 }
--- src/R_affx_parser.cpp
+++ src/R_affx_parser.cpp
@@ -19,20 +19,209 @@
  */
 
 
-#include "CELFileData.h"
-#include "CDFFileData.h"
-#include "CHPFileData.h"
-#include "BARFileData.h"
-#include "BPMAPFileData.h"
-
-using namespace affxcdf;
-using namespace affxcel;
-using namespace affxchp;
-using namespace affxbar;
-using namespace affxbpmap;
-
+#include "FusionCELData.h"
+#include "FusionCDFData.h"
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
+using namespace affymetrix_fusion_io;
 
+
+extern "C" {
+
+  /* uncomment for relatively profuse debugging. */
+  /** #define R_AFFX_DEBUG **/
+
+  #include <R.h>
+  #include <Rdefines.h>  
+ 
+  
+  SEXP R_affx_get_cel_file(SEXP fname, SEXP readHeader, SEXP readOutlierAndMaskedData) 
+  {
+
+    char* celFileName = CHAR(STRING_ELT(fname,0));
+    int readOutlierAndMaskedFlag = INTEGER(readOutlierAndMaskedData)[0];
+    int readHeaderFlag = INTEGER(readHeader)[0];
+   
+    /** here we will store intensity matrix, header, outliers, and masked information. **/
+    SEXP result_list = NEW_LIST(4);
+    
+#ifdef R_AFFX_DEBUG
+    Rprintf("in R_affx_get_cel_file.\n");
+#endif
+
+    FusionCELData cel;
+    /** cel.SetFileName(celFileName); **/ 
+
+#ifdef R_AFFX_DEBUG
+    Rprintf("attempting to read: %s\n", celFileName);
+#endif
+
+    if (cel.ReadEx(celFileName, FusionCELData::CEL_ALL) == false)
+    {
+      Rprintf("Unable to read file: %s\n", celFileName);
+      return R_NilValue;
+    }
+
+    int numCells = cel.GetNumCells();
+
+    SEXP matrix = R_NilValue,dim = R_NilValue,outliers = R_NilValue,
+      masked = R_NilValue, header = R_NilValue, names = R_NilValue;
+
+    int noutlier = 0, nmasked = 0;
+    PROTECT(matrix = NEW_NUMERIC(numCells * 5));
+    
+    if (readOutlierAndMaskedFlag != 0) {
+#ifdef R_AFFX_DEBUG
+      Rprintf("Reading outlier ( %d ) and masked ( %d ) cells.\n", 
+	      cel.GetNumOutliers(), cel.GetNumMasked());
+#endif
+
+      PROTECT(outliers = NEW_INTEGER(cel.GetNumOutliers()));
+      PROTECT(masked   = NEW_INTEGER(cel.GetNumMasked()));
+    }
+
+    for (int icel = 0; icel < numCells; icel++) {
+   	/** this works like this because of the column/row flop. **/
+	REAL(matrix)[icel]              = cel.IndexToX(icel);
+	REAL(matrix)[icel + numCells]   = cel.IndexToY(icel);
+	REAL(matrix)[icel + 2*numCells] = cel.GetIntensity(icel);
+	REAL(matrix)[icel + 3*numCells] = cel.GetStdv(icel);
+	REAL(matrix)[icel + 4*numCells] = cel.GetPixels(icel);
+
+	if (readOutlierAndMaskedFlag != 0) {
+	  if (cel.IsOutlier(icel) == true) {
+	    INTEGER(outliers)[noutlier++] = icel;
+	  }
+
+	  if (cel.IsMasked(icel) == true) {
+	    INTEGER(masked)[nmasked++] = icel;
+	  }
+	}
+    }
+
+#ifdef R_AFFX_DEBUG
+    Rprintf("read cell file intensities.");
+#endif
+  
+    /** Set up the dimensions here we are transposed. **/
+    PROTECT(dim = NEW_INTEGER(2));
+    INTEGER_POINTER(dim)[1] = 5;
+    INTEGER_POINTER(dim)[0] = numCells;
+    SET_DIM(matrix, dim);
+
+    SET_VECTOR_ELT(result_list, 0, header);
+    SET_VECTOR_ELT(result_list, 1, matrix);
+    SET_VECTOR_ELT(result_list, 2, outliers);
+    SET_VECTOR_ELT(result_list, 3, masked);
+
+    /** set up the names of the result list. **/
+    PROTECT(names = NEW_CHARACTER(4));
+    SET_STRING_ELT(names, 0, mkChar("header"));
+    SET_STRING_ELT(names, 1, mkChar("intensities"));
+    SET_STRING_ELT(names, 2, mkChar("outliers"));
+    SET_STRING_ELT(names, 3, mkChar("masked"));
+
+    /** set the names of the list entries. **/
+    setAttrib(result_list, R_NamesSymbol, names);
+
+    if (readOutlierAndMaskedFlag != 0) {
+      UNPROTECT(5);
+    }
+    else {
+      UNPROTECT(3);
+    }
+    return result_list;
+  }
+
+
+  SEXP R_affx_get_pmmm_list(SEXP fname, SEXP complementary_logic) 
+  {
+    FusionCDFData cdf;
+    FusionCDFFileHeader header;
+    SEXP names, dim, pmmm, pairs;
+    int nRows = 0, nCols = 0;
+    char* cdfFileName = CHAR(STRING_ELT(fname, 0));
+
+    cdf.SetFileName(cdfFileName);
+
+#ifdef R_AFFX_DEBUG
+    Rprintf("Attempting to read CDF File: %s\n", cdf.GetFileName().c_str());
+#endif
+
+    if (cdf.Read() == false) {
+      Rprintf("Failed to read the CDF file.");
+      return R_NilValue;
+    }
+
+
+    header = cdf.GetHeader();
+    int nsets = header.GetNumProbeSets();
+
+    PROTECT(names = NEW_CHARACTER(nsets));
+    PROTECT(pmmm  = NEW_LIST(nsets)); 
+
+    nRows = header.GetRows();
+    nCols = header.GetCols();
+
+    for (int iset = 0; iset < nsets; iset++) {
+      const char* name = cdf.GetProbeSetName(iset).c_str();
+
+#ifdef R_AFFX_DEBUG
+    Rprintf("Processing probeset: %s\n", name);
+#endif
+
+      SET_STRING_ELT(names, iset, mkChar(name));
+
+      FusionCDFProbeSetInformation set;
+      cdf.GetProbeSetInformation(iset, set);
+	
+      int ngroups = set.GetNumGroups();
+      for (int igroup = 0; igroup < ngroups; igroup++) {
+	
+	FusionCDFProbeGroupInformation group;
+	set.GetGroupInformation(igroup, group);
+	int ncells = group.GetNumCells();
+	
+	PROTECT(pairs = NEW_INTEGER(ncells));
+	      
+	for (int icell = 0; icell < ncells; icell++) {
+	  FusionCDFProbeInformation probe;
+	  group.GetCell(icell, probe);
+
+	  int x = probe.GetX();
+	  int y = probe.GetY();
+
+	  /**
+	   *return((x - xy.offset) + 1 + nr * (y - xy.offset))
+	   *
+	   *XXX: understand what the offsets do (or just parameterize with them).
+	   *XXX: the groups need to be thought through better.
+	   */
+	   int index = (x + 1) + (nRows * y);
+	   INTEGER(pairs)[icell] = index;
+	}
+
+	/** Set up the dimensions here we are transposed. **/
+	PROTECT(dim = NEW_INTEGER(2));
+	INTEGER_POINTER(dim)[1] = ncells/2;
+	INTEGER_POINTER(dim)[0] = 2;
+	SET_DIM(pairs, dim);
+
+	/** Set the pmmm matrix for this ProbeSet. **/
+	SET_VECTOR_ELT(pmmm, iset, pairs);
+
+	/** pop off dimension, and the pairs matrix. **/
+	UNPROTECT(2);
+      }
+    }
+    /** set the names of the list. **/
+    setAttrib(pmmm, R_NamesSymbol, names);
+
+    /** pop the names, and the vector. **/
+    UNPROTECT(2);
+
+    return pmmm;
+  }
+
+} /** end extern "C" **/
