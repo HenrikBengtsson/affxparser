6e532a11c397fcce3017d8b7c4f25930ed255006
James Bullard <bullard@berkeley.edu>
2005-08-27 19:07:29 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@13754 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.cel.R
+++ R/read.cel.R
@@ -1,3 +1,22 @@
+##  affxparser.R : A utility for parsing Affymetrix files in R.
+##
+##  Copyright (C) 2004-2005 James Bullard, Kasper Daniel Hansen
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU Lesser General Public License as published by
+##  the Free Software Foundation; either version 2.1 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Lesser General Public License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+##  02111-1307  USA
+
 ##
 ## Functions for parsing CEL files
 ##
@@ -6,24 +25,31 @@ read.cel.complete <- function(fname, indices = NULL, read.intensity = TRUE,
                               read.std = TRUE, read.npixels = TRUE,
                               read.xy = FALSE, read.outliers = TRUE,
                               read.masked = TRUE, verbose = FALSE){
-    fname <-fname(path.expand(fname))
-    if(!file.exists(fname))
-        stop(paste(fname, "does not exists."))
-    print("Place holder function")
-    cel.file <- .Call("R_affx_get_cel",
-                     as.character(fname), as.integer(indices),
-                     as.integer(read.intensity), as.integer(read.std),
-                     as.integer(read.npixels), as.integer(read.xy),
-                     as.integer(read.outliers), as.integer(read.masked),
-                     as.integer(verbose))
-    return(cel.file)
+
+
+  cel.file <- .get.cel.file(as.character(fname),
+                            readIntensities = as.integer(read.intensity),
+                            readStdv = as.integer(read.std),
+                            readPixels = as.integer(read.npixels),
+                            readX = as.integer(read.xy),
+                            readY = as.integer(read.xy),
+                            readOutliers = as.integer(read.outliers),
+                            readHeader = TRUE,
+                            readMasked = as.integer(read.masked),
+                            indices = as.integer(indices),
+                            verbose = as.integer(verbose))
+  return(cel.file)
 }
 
 read.cel.header <- function(fname){
-    read.cel.complete(fname = fname, indices = NULL, read.intensity = FALSE,
-                      read.std = FALSE, read.npixels = FALSE,
-                      read.xy = FALSE, read.outliers = FALSE,
-                      read.masked = FALSE, verbose = FALSE)$header
+  fname <- file.path(dirname(fname), basename(fname))
+  
+  if (!file.exists(fname))
+    stop(paste("file:", fname, "does not exist."))
+
+  cel.file.header <- .Call("R_affx_get_cel_file_header", as.character(fname))
+
+  return(cel.file.header)
 }
 
 read.cel.intensity <- function(fnames, indices = NULL, verbose = FALSE){
@@ -32,16 +58,19 @@ read.cel.intensity <- function(fnames, indices = NULL, verbose = FALSE){
     if(verbose)
         cat("Entering read.cel.intensity\n ... reading headers\n")
     all.headers <- lapply(as.list(fnames), read.cel.header)
-    if(length(unique(sapply(all.headers, function(x) x$chipName))) != 1)
-        stop("the associated chipnames are not the same")
-    if(!all(diff(sapply(all.headers, function(x) x$numberOfFeatures))) |
-       !all(diff(sapply(all.headers, function(x) x$numberOfRows))) |
-       !all(diff(sapply(all.headers, function(x) x$numberOfColumns))))
-        stop("dimensions of the files do not match")
+
+    ## XXX: need to fix the reading of the header. 
+    #if(length(unique(sapply(all.headers, function(x) x$chipName))) != 1)
+    #    stop("the associated chipnames are not the same")
+    
+#    if(!all(diff(sapply(all.headers, function(x) x$cols))) |
+#       !all(diff(sapply(all.headers, function(x) x$rows))))
+#        stop("dimensions of the files do not match")
     if(!is.null(indices))
         intensities.nrow <- length(indices)
     else
-        intensities.nrow <- all.headers[[1]]$numberOfFeatures
+        intensities.nrow <-
+          all.headers[[1]]$rows * all.headers[[1]]$rows  # numberOfFeatures
     if(verbose)
         cat(" ...allocating memory for intensity matrix\n") 
     intensities <- matrix(NA, ncol = length(fnames), nrow = intensities.nrow)
@@ -59,4 +88,26 @@ read.cel.intensity <- function(fnames, indices = NULL, verbose = FALSE){
                                               verbose = verbose)$intensity
     }
     return(intensities)
+}
+
+
+.get.cel.file <- function(fname, readHeaders = FALSE, readIntensities =
+                          TRUE, readX = FALSE, readY = FALSE, readPixels
+                          = FALSE, readStdv = FALSE, readOutliers =
+                          FALSE, readMasked = FALSE, indices = NULL,
+                          verbose = 0) {
+  fname <- file.path(dirname(fname), basename(fname))
+  
+  if (!file.exists(fname))
+    stop(paste("file:", fname, "does not exist."))
+
+  cel.file <- .Call("R_affx_get_cel_file", as.character(fname),
+                    as.integer(readHeaders),
+                    as.integer(readIntensities), as.integer(readX),
+                    as.integer(readY), as.integer(readPixels),
+                    as.integer(readStdv), as.integer(readOutliers),
+                    as.integer(readMasked), as.integer(indices),
+                    as.integer(verbose))
+  
+  return(cel.file)
 }
