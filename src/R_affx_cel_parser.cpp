928e0fbf724473f0941546b0696e2b7a6c18ce9a
bullard@berkeley.edu <bullard@berkeley.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-08-30 00:39:12 +0000

    Split up the files because it makes the most sense to do now.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@13778 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_affx_cel_parser.cpp
+++ src/R_affx_cel_parser.cpp
@@ -1,5 +1,5 @@
 /*
- *  R_affx_parser.cpp : A utility for parsing Affymetrix files in R.
+ *  R_affx_cel_parser.cpp : A utility for parsing Affymetrix CEL files in R.
  *
  *  Copyright (C) 2004-2005 James Bullard
  *
@@ -18,28 +18,23 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 #include "FusionCELData.h"
-#include "FusionCDFData.h"
 #include <iostream>
+
+#include "R_affx_constants.h"
 
 using namespace std;
 using namespace affymetrix_fusion_io;
 
 extern "C" {
-
-
   #include <R.h>
   #include <Rdefines.h>  
+  
+  
+
 
   #include <wchar.h>
   #include <wctype.h>
-
-
-  /* uncomment for relatively profuse debugging. */
-  int R_AFFX_VERBOSE = 1;
-  int R_AFFX_REALLY_VERBOSE = 2;
-
  
   /**
    * return a list of the meta data associated with this cell
@@ -359,89 +354,4 @@ extern "C" {
  
     return result_list;
   }
-
-
-  SEXP R_affx_get_pmmm_list(SEXP fname, SEXP complementary_logic, SEXP verbose) 
-  {
-    FusionCDFData cdf;
-    FusionCDFFileHeader header;
-    SEXP names, dim, pmmm, pairs;
-    int nRows = 0, nCols = 0;
-    char* cdfFileName = CHAR(STRING_ELT(fname, 0));
-    int i_verboseFlag = INTEGER(verbose)[0];
-
-    cdf.SetFileName(cdfFileName);
-
-    if (i_verboseFlag == R_AFFX_VERBOSE)
-      Rprintf("Attempting to read CDF File: %s\n", cdf.GetFileName().c_str());
-
-
-    if (cdf.Read() == false) {
-      Rprintf("Failed to read the CDF file.");
-      return R_NilValue;
-    }
-
-    header = cdf.GetHeader();
-    int nsets = header.GetNumProbeSets();
-
-    PROTECT(names = NEW_CHARACTER(nsets));
-    PROTECT(pmmm  = NEW_LIST(nsets)); 
-
-    nRows = header.GetRows();
-    nCols = header.GetCols();
-
-    for (int iset = 0; iset < nsets; iset++) {
-      const char* name = cdf.GetProbeSetName(iset).c_str();
-
-      if (i_verboseFlag == R_AFFX_VERBOSE)
-	Rprintf("Processing probeset: %s\n", name);
-
-
-      SET_STRING_ELT(names, iset, mkChar(name));
-
-      FusionCDFProbeSetInformation set;
-      cdf.GetProbeSetInformation(iset, set);
-	
-      int ngroups = set.GetNumGroups();
-      for (int igroup = 0; igroup < ngroups; igroup++) {
-	
-	FusionCDFProbeGroupInformation group;
-	set.GetGroupInformation(igroup, group);
-	int ncells = group.GetNumCells();
-	
-	PROTECT(pairs = NEW_INTEGER(ncells));
-	      
-	for (int icell = 0; icell < ncells; icell++) {
-	  FusionCDFProbeInformation probe;
-	  group.GetCell(icell, probe);
-
-	  int x = probe.GetX();
-	  int y = probe.GetY();
-
-	  int index = (x + 1) + (nRows * y);
-	  INTEGER(pairs)[icell] = index;
-	}
-
-	/** Set up the dimensions here we are transposed. **/
-	PROTECT(dim = NEW_INTEGER(2));
-	INTEGER_POINTER(dim)[1] = ncells/2;
-	INTEGER_POINTER(dim)[0] = 2;
-	SET_DIM(pairs, dim);
-
-	/** Set the pmmm matrix for this ProbeSet. **/
-	SET_VECTOR_ELT(pmmm, iset, pairs);
-
-	/** pop off dimension, and the pairs matrix. **/
-	UNPROTECT(2);
-      }
-    }
-    /** set the names of the list. **/
-    setAttrib(pmmm, R_NamesSymbol, names);
-
-    /** pop the names, and the vector. **/
-    UNPROTECT(2);
-
-    return pmmm;
-  }
-
 } /** end extern "C" **/
