d3222d85ef42718cb65ab9cce825a9ecfcd876f3
bullard@berkeley.edu <bullard@berkeley.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-08-30 04:45:50 +0000

    Added some support for the parsing of the whole cdf environment.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@13780 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_affx_cdf_parser.cpp
+++ src/R_affx_cdf_parser.cpp
@@ -45,7 +45,7 @@ extern "C" {
 
     cdf.SetFileName(cdfFileName);
 
-    if (i_verboseFlag == R_AFFX_VERBOSE)
+    if (i_verboseFlag >= R_AFFX_VERBOSE)
       Rprintf("Attempting to read CDF File: %s\n", cdf.GetFileName().c_str());
 
 
@@ -66,7 +66,7 @@ extern "C" {
     for (int iset = 0; iset < nsets; iset++) {
       const char* name = cdf.GetProbeSetName(iset).c_str();
 
-      if (i_verboseFlag == R_AFFX_VERBOSE)
+      if (i_verboseFlag >= R_AFFX_VERBOSE)
 	Rprintf("Processing probeset: %s\n", name);
 
 
@@ -127,16 +127,26 @@ extern "C" {
   {
     FusionCDFData cdf;
     FusionCDFFileHeader header;
-    SEXP names, dim, pmmm, pairs;
-    int nRows = 0, nCols = 0;
+
+    SEXP names = R_NilValue, 
+      probe_sets = R_NilValue,
+      cell_list = R_NilValue,
+      cell_list_names = R_NilValue,
+      xvals = R_NilValue,
+      yvals = R_NilValue,
+      pbase = R_NilValue,
+      tbase = R_NilValue,
+      expos = R_NilValue;
+    
+
+    int nsets = 0; 
     char* cdfFileName = CHAR(STRING_ELT(fname, 0));
     int i_verboseFlag = INTEGER(verbose)[0];
 
     cdf.SetFileName(cdfFileName);
 
-    if (i_verboseFlag == R_AFFX_VERBOSE)
+    if (i_verboseFlag >= R_AFFX_VERBOSE)
       Rprintf("Attempting to read CDF File: %s\n", cdf.GetFileName().c_str());
-
 
     if (cdf.Read() == false) {
       Rprintf("Failed to read the CDF file.");
@@ -144,21 +154,18 @@ extern "C" {
     }
 
     header = cdf.GetHeader();
-    int nsets = header.GetNumProbeSets();
+    nsets = header.GetNumProbeSets();
 
     PROTECT(names = NEW_CHARACTER(nsets));
-    PROTECT(pmmm  = NEW_LIST(nsets)); 
-
-    nRows = header.GetRows();
-    nCols = header.GetCols();
+    PROTECT(probe_sets = NEW_LIST(nsets)); 
 
     for (int iset = 0; iset < nsets; iset++) {
       const char* name = cdf.GetProbeSetName(iset).c_str();
 
-      if (i_verboseFlag == R_AFFX_VERBOSE)
+      if (i_verboseFlag >= R_AFFX_VERBOSE)
 	Rprintf("Processing probeset: %s\n", name);
-
-
+      
+      /** the probe set names. **/
       SET_STRING_ELT(names, iset, mkChar(name));
 
       FusionCDFProbeSetInformation set;
@@ -166,44 +173,81 @@ extern "C" {
 	
       int ngroups = set.GetNumGroups();
       for (int igroup = 0; igroup < ngroups; igroup++) {
-	
 	FusionCDFProbeGroupInformation group;
 	set.GetGroupInformation(igroup, group);
-	int ncells = group.GetNumCells();
 	
-	PROTECT(pairs = NEW_INTEGER(ncells));
-	      
+	int ncells = group.GetNumCells();
+	int unp = 0, n_list_elts = 5; 
+
+	/** XXX: need to understand what this list contains. **/
+	PROTECT(cell_list = NEW_LIST(n_list_elts));
+	PROTECT(cell_list_names = NEW_STRING(n_list_elts));
+
+	PROTECT(xvals = NEW_INTEGER(ncells));
+	PROTECT(yvals = NEW_INTEGER(ncells));
+	PROTECT(pbase = NEW_STRING(ncells));
+	PROTECT(tbase = NEW_STRING(ncells));
+	PROTECT(expos = NEW_INTEGER(ncells));
+
 	for (int icell = 0; icell < ncells; icell++) {
 	  FusionCDFProbeInformation probe;
 	  group.GetCell(icell, probe);
 
-	  int x = probe.GetX();
-	  int y = probe.GetY();
+	  if (i_verboseFlag >= R_AFFX_REALLY_VERBOSE)
+	    Rprintf("icell: %d x: %d, y: %d, pbase: %c, tbase: %c, expos: %d\n", 
+		    icell, probe.GetX(), probe.GetY(), probe.GetPBase(), probe.GetTBase(),
+		    probe.GetExpos());
+	  
+	  INTEGER(xvals)[icell] = probe.GetX();
+	  INTEGER(yvals)[icell] = probe.GetY();
 
-	  int index = (x + 1) + (nRows * y);
-	  INTEGER(pairs)[icell] = index;
+	  SET_STRING_ELT(pbase, icell, mkChar(CHARACTER_POINTER(probe.GetPBase())));
+	  SET_STRING_ELT(tbase, icell, mkChar(CHARACTER_POINTER(probe.GetTBase())));
+	  
+	  /*	  CHAR(pbase)[icell] = probe.GetPBase();
+		  CHAR(tbase)[icell] = probe.GetTBase(); */
+	  INTEGER(expos)[icell] = probe.GetExpos(); 
 	}
 
-	/** Set up the dimensions here we are transposed. **/
-	PROTECT(dim = NEW_INTEGER(2));
-	INTEGER_POINTER(dim)[1] = ncells/2;
-	INTEGER_POINTER(dim)[0] = 2;
-	SET_DIM(pairs, dim);
+	if (i_verboseFlag >= R_AFFX_VERBOSE)
+	  Rprintf("finished reading probeset information for: %s\n", name);
 
-	/** Set the pmmm matrix for this ProbeSet. **/
-	SET_VECTOR_ELT(pmmm, iset, pairs);
+	/** do I have to make the attribute vector everytime? **/
+	SET_VECTOR_ELT(cell_list, unp, xvals);
+	SET_STRING_ELT(cell_list_names, unp++, mkChar("x"));
+ 
+	SET_VECTOR_ELT(cell_list, unp, yvals);
+	SET_STRING_ELT(cell_list_names, unp++, mkChar("y"));
+	
+	SET_VECTOR_ELT(cell_list, unp, pbase);
+	SET_STRING_ELT(cell_list_names, unp++, mkChar("pbase"));
+ 
+	SET_VECTOR_ELT(cell_list, unp, tbase);
+	SET_STRING_ELT(cell_list_names, unp++, mkChar("tbase"));
 
-	/** pop off dimension, and the pairs matrix. **/
-	UNPROTECT(2);
+	SET_VECTOR_ELT(cell_list, unp, expos);
+	SET_STRING_ELT(cell_list_names, unp++, mkChar("expos"));
+	
+
+	/** set the names of the new list, dont really know if I need to do
+	    this each and every time. **/
+	setAttrib(cell_list, R_NamesSymbol, cell_list_names);
+	  
+	/** unprotect the vectors stored in our list. **/
+	UNPROTECT(unp + 2);
       }
+
+      /** now set the cell_list for each probe set. **/
+      SET_VECTOR_ELT(probe_sets, iset, cell_list);
     }
-    /** set the names of the list. **/
-    setAttrib(pmmm, R_NamesSymbol, names);
+    
+    /** set the names down here at the end. **/
+    setAttrib(probe_sets, R_NamesSymbol, names);
 
-    /** pop the names, and the vector. **/
+    /** unprotect the names and the main probe set list.**/
     UNPROTECT(2);
-
-    return pmmm;
+    
+    return probe_sets;
   }
 
 } /** end extern C **/
