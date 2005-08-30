1bc256162c126902639694e119577594decadd6f
James Bullard <extemporaneousb@yahoo.com>
2005-08-30 04:45:50 +0000

    Added some support for the parsing of the whole cdf environment.

--- R/read.cdf.R
+++ R/read.cdf.R
@@ -1,8 +1,9 @@
-read.cdf.complete <- function(fname){
-    fname <- path.expand(fname)
-    if(!file.exists(fname))
-        stop(paste(fname, "does not exist."))
-    print("Placeholder function")
+read.cdf.complete <- function(fname, verbose = 0){
+  fname <- path.expand(fname)
+  if(!file.exists(fname))
+    stop(paste(fname, "does not exist."))
+
+  .Call("R_affx_get_cdf_file", as.character(fname), as.integer(verbose))
 }
 
 read.cdf.header <- function(fname){
@@ -10,3 +11,24 @@ read.cdf.header <- function(fname){
     print("Placeholder function")
 }
 
+
+read.cdf.env <- function(fname) {
+  pmmm <- .parse.cdffile(fname, complementary.logic)
+  e <- new.env()
+  multiassign(names(pmmm), value = pmmm, e)
+  e
+}
+
+.get.cdf.env <- function(fname) {
+  fname <- file.path(dirname(fname), basename(fname))
+
+  if (!file.exists(fname))
+    stop(paste("file:", fname, "does not exist."))
+
+  pmmm <- .Call("R_affx_get_pmmm_list", as.character(fname), as.integer(complementary.logic))
+
+  if (is.null(pmmm) || length(pmmm) == 0)
+    stop(paste("Error parsing:", fname))
+  
+  lapply(pmmm, t)
+}
