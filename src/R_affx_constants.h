499203fc45684589b2e2dcaa5d05d32f256e0e73
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2005-09-08 05:50:13 +0000

    Increased the version number. Deleted all swig directories. Detailed description of all files/directories removed in affxparser/inst/README (perhaps want to put this in another file). Changed R_affx_constants.h and Makevars to fix endian issue. Included removal of *.o and *.so in _Makefile target:clean. Copied the fusion license over (it is just LGLP2, but just to be sure ...).
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@13961 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_affx_constants.h
+++ src/R_affx_constants.h
@@ -21,3 +21,14 @@
 
 #define R_AFFX_VERBOSE 1
 #define R_AFFX_REALLY_VERBOSE 2
+
+/*
+ * Using R's test of endianness
+ */
+
+#include <Rconfig.h>
+
+#ifdef WORDS_BIGENDIAN
+  #define IS_BIG_ENDIAN 1
+#endif
+
