15751fd43f197a09bda9076f0693a3a07b761a21
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2005-09-16 20:55:21 +0000

    Had to change some license file.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@14119 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/affxparserInfo.R
+++ R/affxparserInfo.R
@@ -1,4 +1,13 @@
-affxparserInfo <- function(){
-    writeLines(readLines(system.file("README", package = "affxparser"),
+affxparserInfo <- function(fusion.license = FALSE,
+                           file.changes = FALSE){
+    writeLines(readLines(system.file("info/README",
+                                     package = "affxparser"),
                          n = -1))
+    if(file.changes)
+        writeLines(readLines(system.file("info/CHANGES",
+                                         package = "affxparser"),
+                             n = -1))
+    if(fusion.license)
+        browseURL(system.file("info/fusion_sdk_license.html",
+                                     package = "affxparser"))
 }
