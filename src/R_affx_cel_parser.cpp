9766df82767090b06bda6cc7642507d07dd03189
bullard@berkeley.edu <bullard@berkeley.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-01-25 03:11:39 +0000

    Changed the read_cel_meta function to take a pointer rather than the object itself - don't know how this one slipped by us.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@15859 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_affx_cel_parser.cpp
+++ src/R_affx_cel_parser.cpp
@@ -36,7 +36,7 @@ extern "C" {
    * return a list of the meta data associated with this cell
    * file. This is the information stored in the header. 
    */
-  SEXP R_affx_extract_cel_file_meta(FusionCELData cel) {
+  SEXP R_affx_extract_cel_file_meta(FusionCELData* cel) {
     SEXP names, vals;
 
     PROTECT(names = NEW_CHARACTER(12));
@@ -46,26 +46,26 @@ extern "C" {
     SEXP tmp;
 
     SET_STRING_ELT(names, i, mkChar("filename"));
-    SET_VECTOR_ELT(vals, i++, mkString(cel.GetFileName().c_str()));
+    SET_VECTOR_ELT(vals, i++, mkString(cel->GetFileName().c_str()));
 
     SET_STRING_ELT(names, i, mkChar("version"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetVersion();
+    INTEGER(tmp)[0] = cel->GetVersion();
     SET_VECTOR_ELT(vals, i++, tmp); 
 
     SET_STRING_ELT(names, i, mkChar("cols"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetCols();
+    INTEGER(tmp)[0] = cel->GetCols();
     SET_VECTOR_ELT(vals, i++, tmp);
 
     SET_STRING_ELT(names, i, mkChar("rows"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetRows();
+    INTEGER(tmp)[0] = cel->GetRows();
     SET_VECTOR_ELT(vals, i++, tmp);
     
     SET_STRING_ELT(names, i, mkChar("total"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetNumCells();
+    INTEGER(tmp)[0] = cel->GetNumCells();
     SET_VECTOR_ELT(vals, i++, tmp);
    
 
@@ -73,30 +73,30 @@ extern "C" {
     int str_length; 
     char* cstr; 
     
-    str_length = cel.GetAlg().size();
+    str_length = cel->GetAlg().size();
     cstr = Calloc(str_length, char);
-    wcstombs(cstr, cel.GetAlg().c_str(), str_length);
+    wcstombs(cstr, cel->GetAlg().c_str(), str_length);
     SET_STRING_ELT(names, i, mkChar("algorithm"));
     SET_VECTOR_ELT(vals, i++, mkString(cstr)); 
     Free(cstr);
 
-    str_length = cel.GetParams().size();
+    str_length = cel->GetParams().size();
     cstr = Calloc(str_length, char);
-    wcstombs(cstr, cel.GetParams().c_str(), str_length);
+    wcstombs(cstr, cel->GetParams().c_str(), str_length);
     SET_STRING_ELT(names, i, mkChar("parameters"));
     SET_VECTOR_ELT(vals, i++, mkString(cstr));
     Free(cstr);
    
-    str_length = cel.GetChipType().size();
+    str_length = cel->GetChipType().size();
     cstr = Calloc(str_length, char);
-    wcstombs(cstr, cel.GetChipType().c_str(), str_length);
+    wcstombs(cstr, cel->GetChipType().c_str(), str_length);
     SET_STRING_ELT(names, i, mkChar("chiptype"));
     SET_VECTOR_ELT(vals, i++, mkString(cstr));
     Free(cstr);
 	
-    str_length = cel.GetHeader().size();
+    str_length = cel->GetHeader().size();
     cstr = Calloc(str_length, char);
-    wcstombs(cstr, cel.GetHeader().c_str(), str_length);
+    wcstombs(cstr, cel->GetHeader().c_str(), str_length);
     SET_STRING_ELT(names, i, mkChar("header"));
     SET_VECTOR_ELT(vals, i++, mkString(cstr));
     Free(cstr);
@@ -119,17 +119,17 @@ extern "C" {
 
     SET_STRING_ELT(names, i, mkChar("cellmargin"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetCellMargin();
+    INTEGER(tmp)[0] = cel->GetCellMargin();
     SET_VECTOR_ELT(vals, i++, tmp);
     
     SET_STRING_ELT(names, i, mkChar("noutliers"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetNumOutliers();
+    INTEGER(tmp)[0] = cel->GetNumOutliers();
     SET_VECTOR_ELT(vals, i++, tmp);
 
     SET_STRING_ELT(names, i, mkChar("nmasked"));
     tmp = allocVector(INTSXP, 1);
-    INTEGER(tmp)[0] = cel.GetNumMasked();
+    INTEGER(tmp)[0] = cel->GetNumMasked();
     SET_VECTOR_ELT(vals, i++, tmp);
 
     setAttrib(vals, R_NamesSymbol, names);
@@ -150,7 +150,7 @@ extern "C" {
       return R_NilValue;
     }
     else {
-      return  R_affx_extract_cel_file_meta(cel);
+      return  R_affx_extract_cel_file_meta(&cel);
     }
   }
 
@@ -230,7 +230,7 @@ extern "C" {
    
     /** conditionally allocate space for each vector we want to return. **/
     if (i_readHeader != 0) {
-      header = R_affx_extract_cel_file_meta(cel);
+      header = R_affx_extract_cel_file_meta(&cel);
     }
     if (i_readIntensities != 0) {
       PROTECT(intensities = NEW_NUMERIC(numCells));
