6a5d9e43c8f8f62921aeb9d2787c33b934610374
James Bullard <extemporaneousb@yahoo.com>
2006-02-05 18:28:05 +0000

    This revision changes the names from the 'old-style' dot syntax to the 'new-style camel case syntax. This change was made after fierce negotiations and a bribe. Now no publicly available method should use the '.' syntax. Now, also Henrik's changes should be fully integrated into the package.

--- R/readCdfUnits.R
+++ R/readCdfUnits.R
@@ -1,55 +1,4 @@
-#########################################################################/**
-# @RdocFunction readCdfUnits
-# @alias read.cdf.units
-#
-# @title "Reads units (probesets) from an Affymetrix CDF file"
-#
-# @synopsis 
-# 
-# \description{
-#   Gets the all or a subset of units (probesets) in an Affymetrix CDF file.
-# }
-# 
-# \arguments{
-#   \item{filename}{The filename of the CDF file.}
-#   \item{units}{An @integer @vector of (zero-based) unit indices 
-#     specifying which units to be read.  If @NULL, all units are read.}
-#   \item{verbose}{An @integer specifying the verbose level. If 0, the 
-#     file is parsed quietly.  The higher numbers, the more details.}
-# }
-# 
-# \value{
-#   A named @list where the names corresponds to the names of the units
-#   read.  Each element of the @list is in turn a @list structure 
-#   with three components:
-#    \item{groups}{A @list with one component for each group (also called
-#      block). The information on each group is a @list with five 
-#      components: \code{x}, \code{y}, \code{pbase}, \code{tbase},
-#      \code{expos}.}
-#    \item{type}{An @integer specifying the type of the unit.}
-#    \item{direction}{An @integer specifying the direction of the unit.}
-# }
-#
-# \author{James Bullard, \email{bullard@stat.berkeley.edu} and Kasper
-#   Daniel Hansen, \email{khansen@stat.berkeley.edu}.
-#   Modified by Henrik Bengtsson, \email{hb@maths.lth.se}, to read
-#   any subset of units.} 
-# 
-# \examples{\dontrun{@include "readCdfUnits.Rex"}}
-# 
-# \seealso{
-#   @see "readCdfUnitNames".
-# }
-# 
-# \references{
-#   [1] Affymetrix Inc, Affymetrix GCOS 1.x compatible file formats,
-#       June 14, 2005.
-#       \url{http://www.affymetrix.com/support/developer/}
-# }
-#
-# @keyword "file"
-# @keyword "IO"
-#*/#########################################################################
+
 readCdfUnits <- function(filename, units=NULL, verbose=0) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   # Validate arguments
@@ -80,19 +29,6 @@ readCdfUnits <- function(filename, units=NULL, verbose=0) {
   # Read the CDF file
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   .Call("R_affx_get_cdf_units", filename, units, verbose, 
-                                                     PACKAGE="affxparser");
+        PACKAGE="affxparser");
 }
 
-read.cdf.units <- function(filename, units=NULL, verbose=0) {
-  readCdfUnits(filename=filename, units=units, verbose=verbose);
-}
-
-
-############################################################################
-# HISTORY:
-# 2006-01-09
-# o Note that ../man/readCdfUnits.R in generated from the Rdoc comments
-#   above.  See the R.oo package for details.  Don't remove the *.Rex files!
-# o Created by HB.  The purpose was to make it possible to read subsets
-#   of units and not just all units at once.  /HB
-############################################################################ 
