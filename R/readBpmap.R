80debc4476e48737e541db5dd682689643943899
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-05-23 06:00:02 +0000

    Forgot to fix the Makevars.win file after I added some BPMAP stuff. Should compile on Windows now. I also removed the test function read.affybatch2. Finally a small update to the BPMAP parser - it is now essentially complete, including the bug fix mentioned in last commit.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@18185 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readBpmap.R
+++ R/readBpmap.R
@@ -1,4 +1,11 @@
-readBpmap <- function(filename, seqIndices = NULL, verbose = 0){
-    .Call("R_affx_get_bpmap_file", filename, as.integer(seqIndices), as.integer(verbose),
-          PACKAGE = "affxparser");
+readBpmap <- function(filename, seqIndices = NULL, readProbeSeq = TRUE,
+                      readSeqInfo = TRUE, readPMXY = TRUE,
+                      readMMXY = TRUE, readStartPos = TRUE,
+                      readCenterPos = FALSE, readStrand = TRUE,
+                      readMatchScore = FALSE, readProbeLength = FALSE,
+                      verbose = 0){
+    .Call("R_affx_get_bpmap_file", filename, as.integer(seqIndices),
+          readSeqInfo, readStartPos, readCenterPos, readProbeSeq,
+          readStrand, readPMXY, readMMXY, readMatchScore, readProbeLength,
+          as.integer(verbose), PACKAGE = "affxparser");
 }
