a87bd555198cb041f6cbd1feeaac71aab7480c23
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-09-19 22:22:36 +0000

    Added some man pages and did a bug fix to readBpmap. I also removed the affxparserInfo thing.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@19958 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readBpmapSeqInfo.R
+++ R/readBpmapSeqInfo.R
@@ -1,4 +1,4 @@
-readBpmapSeqInfo <- function(filename, seqIndices = NULL, verbose = 0){
+readBpmapSeqinfo <- function(filename, seqIndices = NULL, verbose = 0){
     seqInfo <- .Call("R_affx_get_bpmap_seqinfo", filename, as.integer(seqIndices),
                      as.integer(verbose), PACKAGE = "affxparser");
     seqInfo
