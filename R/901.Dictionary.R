d223eaa3df15eb8925d59a0154d1493d6aed79b5
Henrik Bengtsson <hb@maths.lth.se>
2006-09-25 10:49:21 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@20081 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/901.Dictionary.R
+++ R/901.Dictionary.R
@@ -8,18 +8,19 @@
 #   \item{affxparser}{The name of this package.}
 #   \item{API}{Application program interface, which describes the 
 #     functional interface of underlying methods.}
-#   \item{BPMAP}{An Affymetrix file format: contains information
-#      related to the design of the Affymetrix tiling arrays.}
-#   \item{DAT}{An Affymetrix file format: contains pixel intensity
-#      values collected from an Affymetrix GeneArray scanner.}
-#   \item{Calvin}{A special Affymetrix binary file format.}
-#   \item{CDF}{An Affymetrix file format: chip definition file.}
-#   \item{CEL}{An Affymetrix file format: cell intensity file.}
+#   \item{block}{(aka group).}
+#   \item{BPMAP}{A file format containing information
+#      related to the design of the tiling arrays.}
+#   \item{Calvin}{A special binary file format.}
+#   \item{CDF}{A file format: chip definition file.}
+#   \item{CEL}{A file format: cell intensity file.}
 #   \item{cell}{(aka feature) A probe.}
 #   \item{cell index}{An integer that identifies a probe uniquely.}
 #   \item{chip}{An array.}
-#   \item{chip type}{An identifier specifying a Affymetrix chip design 
+#   \item{chip type}{An identifier specifying a chip design 
 #     uniquely, e.g. \code{"Mapping50K_Xba240"}.}
+#   \item{DAT}{A file format: contains pixel intensity
+#      values collected from an Affymetrix GeneArray scanner.}
 #   \item{feature}{A probe.}
 #   \item{Fusion SDK}{Open-source sofware development kit (SDK) provided
 #     by Affymetrix to access their data files.}
@@ -28,10 +29,12 @@
 #     Expression arrays typically only have one group per unit, whereas
 #     SNP arrays have either two or four groups per unit, one for each of
 #     the two allele times possibly repeated for both strands.}
-#   \item{PM}{Perfect-match, e.g. PM probe.}
 #   \item{MM}{Mismatch-match, e.g. MM probe.}
+#   \item{TPMAP}{A file format storing the relationship between (PM,MM) 
+#     pairs (or PM probes) and positions on a set of sequences.}
 #   \item{QC}{Quality control, e.g. QC probes and QC probe sets.}
 #   \item{unit}{A probeset.}
+#   \item{XDA}{A file format, aka as the binary file format.}
 #  }
 # }
 #
