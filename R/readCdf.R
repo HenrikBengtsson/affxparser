b7c7fe79325e7d62e769549dd27df1cb34c95d8f
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-11-20 20:27:35 +0000

    Added stratifyBy argument to readCdf and found and fixed a little bug when calling readCdf with readIndices = FALSE (which was actually not possible in the previous version). The stratifyBy argument may have a slight overhead since we need to take care of stuf such as groupdirection.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@21232 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readCdf.R
+++ R/readCdf.R
@@ -1,25 +1,164 @@
-readCdf <- function(filename, units = NULL, verbose = 0){
-  filename <- file.path(dirname(filename), basename(filename))
-  if (!file.exists(filename))
-    stop("file:", filename, "does not exist.")
+readCdf <- function(filename, units=NULL, readXY=TRUE, readBases=TRUE,
+                    readIndexpos=TRUE, readAtoms = TRUE,
+                    readUnitType=TRUE, readUnitDirection=TRUE,
+                    readUnitNumber = FALSE, readUnitAtomNumbers = FALSE,
+                    readGroupAtomNumbers = FALSE, readGroupDirection = FALSE,
+                    stratifyBy=c("nothing", "pmmm", "pm", "mm"),
+                    readIndices=FALSE, verbose=0) {
+    filename <- file.path(dirname(filename), basename(filename))
+    if (!file.exists(filename))
+        stop("File not found: ", filename)
+    ## Argument 'units':
+    if (! (is.null(units) || is.numeric(units))) {
+        stop("Argument 'units' must be numeric or NULL: ", class(units)[1])
+    }
+    ## other arguments
+    stratifyBy <- match.arg(stratifyBy)
+    returnUnitType <- as.logical(readUnitType)
+    returnUnitDirection <- as.logical(readUnitDirection)
+    returnUnitAtomNumbers <- as.logical(readUnitAtomNumbers)
+    returnUnitNumber <- as.logical(readUnitNumber)
+    returnXY <- as.logical(readXY)
+    returnIndices <- as.logical(readIndices)
+    returnBases <- as.logical(readBases)
+    returnAtoms <- as.logical(readAtoms)
+    returnIndexpos <- as.logical(readIndexpos)
+    returnBlockDirection <- as.logical(readGroupDirection)
+    returnBlockAtomNumbers <- as.logical(readGroupAtomNumbers)
+    cdf <- .Call("R_affx_get_cdf_file",
+                 filename, as.integer(units), as.integer(verbose),
+                 returnUnitType, returnUnitDirection,
+                 returnUnitAtomNumbers, returnUnitNumber,
+                 returnXY, returnIndices,
+                 returnBases, returnAtoms, returnIndexpos,
+                 returnBlockDirection, returnBlockAtomNumbers,
+                 PACKAGE="affxparser")
+    if (stratifyBy == "nothing")
+        return(cdf);
 
-  returnUnitType <- TRUE
-  returnUnitDirection <- TRUE
-  returnUnitAtomNumbers <- TRUE
-  returnUnitNumber <- TRUE
-  returnXY <- TRUE
-  returnIndices <- TRUE
-  returnBases <- TRUE
-  returnAtoms <- TRUE
-  returnIndexpos <- TRUE
-  returnBlockDirection <- TRUE
-  returnAtomNumbers <- TRUE
-  return(.Call("R_affx_get_cdf_file",
-               filename, as.integer(units), as.integer(verbose),
-               returnUnitType, returnUnitDirection,
-               returnUnitAtomNumbers, returnUnitNumber,
-               returnXY, returnIndices,
-               returnBases, returnAtoms, returnIndexpos,
-               returnBlockDirection, returnAtomNumbers,
-               PACKAGE="affxparser"))
-}
+    ## Now we stratify
+    isPm <- readCdfIsPm(filename, units=units);
+
+    ## Using .subset2() instead of "[["() to avoid dispatching overhead etc.
+    if (stratifyBy == "pmmm") {
+        dimnames <- list(c("pm", "mm"), NULL);
+        
+        for (uu in seq(along=cdf)) {
+            groups <- .subset2(.subset2(cdf, uu), "groups");
+            ngroups <- length(groups);
+            if (ngroups == 0)
+                next;
+
+            for (gg in 1:ngroups) {
+                group <- .subset2(groups, gg);
+                groupNoStratifyNames <- intersect(c("groupdirection",
+                                                  "natoms", "ncellsperatom"),
+                                                names(group))
+                if(length(groupNoStratifyNames) > 0) {
+                    groupNoStratifyElements <- group[groupNoStratifyNames]
+                    group <- group[setdiff(names(group), c("groupdirection",
+                                                  "natoms", "ncellsperatom"))]
+                }
+                pm <- .subset2(.subset2(isPm, uu), gg);
+                idx <- 1:length(pm);
+                mm <- idx[!pm];  # Note: which(!pm) is about 60% slower! /HB
+                pm <- idx[pm];
+                npm <- length(pm);
+                if (npm != length(mm)) {
+                    ## This is not expected to happen, but just in case.
+                    stop("Number of PM and MM probes differ in probeset #", uu,
+                         ": ", length(pm), " != ", length(mm));
+                }
+                pmmm <- .Internal(matrix(c(pm, mm), 2, npm, TRUE));
+
+                ## Re-order cell elements according to PM/MM.
+                ngroup <- length(group);
+                if (ngroup > 0) {
+                    dim <- c(2, npm);
+                    for (kk in 1:ngroup) {
+                        ## value <- group[[kk]][pmmm];
+                        value <- .subset(.subset2(group, kk), pmmm);
+                        dim(value) <- dim; 
+                        dimnames(value) <- dimnames;
+                        group[[kk]] <- value;
+                    }
+                }
+                
+                if(length(groupNoStratifyNames) > 0) {
+                    group <- c(group, groupNoStratifyElements)
+                }
+                groups[[gg]] <- group;
+            } # for (gg ...)
+            cdf[[uu]]$groups <- groups;
+        } # for (uu ...)
+    } else if (stratifyBy == "pm") {
+        for (uu in seq(along=cdf)) {
+            groups <- cdf[[uu]]$groups;
+            ngroups <- length(groups);
+            if (ngroups == 0)
+                next;
+
+            for (gg in 1:ngroups) {
+                group <- groups[[gg]];
+                groupNoStratifyNames <- intersect(c("groupdirection",
+                                                  "natoms", "ncellsperatom"),
+                                                names(group))
+                if(length(groupNoStratifyNames) > 0) {
+                    groupNoStratifyElements <- group[groupNoStratifyNames]
+                    group <- group[setdiff(names(group), c("groupdirection",
+                                                  "natoms", "ncellsperatom"))]
+                }
+                ngroup <- length(group);
+                if (ngroup == 0)
+                    next;
+                
+                pm <- .subset2(.subset2(isPm, uu), gg);
+                pm <- (1:length(pm))[pm]; # Note: which(!pm) is about 60% slower!
+                for (kk in 1:ngroup) {
+                    group[[kk]] <- .subset(.subset2(group, kk), pm);
+                }
+                if(length(groupNoStratifyNames) > 0) {
+                    group <- c(group, groupNoStratifyElements)
+                }
+                groups[[gg]] <- group;
+            } # for (gg ...)
+            cdf[[uu]]$groups <- groups;
+        } # for (uu ...)
+    } else if (stratifyBy == "mm") {
+        for (uu in seq(along=cdf)) {
+            groups <- cdf[[uu]]$groups;
+            ngroups <- length(groups);
+            if (ngroups == 0)
+                next;
+            
+            for (gg in 1:ngroups) {
+                group <- groups[[gg]];
+                groupNoStratifyNames <- intersect(c("groupdirection",
+                                                  "natoms", "ncellsperatom"),
+                                                names(group))
+                if(length(groupNoStratifyNames) > 0) {
+                    groupNoStratifyElements <- group[groupNoStratifyNames]
+                    group <- group[setdiff(names(group), c("groupdirection",
+                                                  "natoms", "ncellsperatom"))]
+                }
+                ngroup <- length(group);
+                if (ngroup == 0)
+                    next;
+                
+                pm <- .subset2(.subset2(isPm, uu), gg);
+                mm <- (1:length(pm))[!pm]; # Note: which(!pm) is about 60% slower!
+                for (kk in 1:ngroup) {
+                    group[[kk]] <- .subset(.subset2(group, kk), mm);
+                }
+                if(length(groupNoStratifyNames) > 0) {
+                    group <- c(group, groupNoStratifyElements)
+                }
+                groups[[gg]] <- group;
+            } # for (gg ...)
+            cdf[[uu]]$groups <- groups;
+        } # for (uu ...)
+    }
+    
+    cdf;
+} # readCdfUnits()
+
