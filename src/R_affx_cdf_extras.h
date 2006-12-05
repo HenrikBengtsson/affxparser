303d72f5b693f8cdd7d78f149e1754bcbccd5b57
James Bullard <bullard@berkeley.edu>
2006-12-05 17:32:53 +0000

    Added the R_affx_cdf_extras.h - unfortunately this addition should have been associated with my last commit (the commit of the patch adding isPm functionality), however because I am slow in the morning it did not.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@21349 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/R_affx_cdf_extras.h
@@ -0,0 +1,8 @@
+#if !defined(R_AFFX_CDF_EXTRAS_H)
+#define R_AFFX_CDF_EXTRAS_H
+extern "C" {
+    /* utility functions that can be used elsewhere in the code */
+int R_affx_pt_base_is_pm(char p_base, char t_base);
+
+}
+#endif
