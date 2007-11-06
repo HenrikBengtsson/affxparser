06d255b8f4f1dcbead01fc10cf39a2b44c2ba546
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-11-06 06:26:55 +0000

    Fixed small as.integer() bug in tpmap2bpmap
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@28489 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tpmap2bpmap.R
+++ R/tpmap2bpmap.R
@@ -1,3 +1,3 @@
 tpmap2bpmap <- function(tpmapname, bpmapname, verbose = 0){
-    invisible(.Call("R_affx_write_bpmap_file", bpmapname, tpmapname, verbose))
+    invisible(.Call("R_affx_write_bpmap_file", bpmapname, tpmapname, as.integer(verbose)))
 }
