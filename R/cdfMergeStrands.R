a42c6a76bdabef954c2b0c44d554fc68b2fd9922
Henrik Bengtsson <hb@maths.lth.se>
2008-02-22 19:30:24 +0000

    # o Updated cdfMergeStrands() to also handle six, eight, ... groups.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@30114 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cdfMergeStrands.R
+++ R/cdfMergeStrands.R
@@ -46,42 +46,57 @@
 #**/#######################################################################
 cdfMergeStrands <- function(groups, ...) {
   nbrOfGroups <- length(groups);
-  if (nbrOfGroups != 2 && nbrOfGroups != 4)
+  if (nbrOfGroups != 2 && nbrOfGroups != 4 && nbrOfGroups %% 2 != 0)
     return(groups);
 
   names <- names(groups);
   unames <- unique(names);
 
   res <- list();
+
+  # For each allele...
   for (name in unames) {
     idx <- which(name == names);
-    group1 <- groups[[idx[1]]];
-    group2 <- groups[[idx[2]]];
-    for (kk in seq(along=group1)) {
-      fields1 <- group1[[kk]];
-      fields2 <- group2[[kk]];
-      ndim <- length(dim(fields1));
-      if (ndim <= 1) {
-        fields1 <- append(fields1, fields2);
-      } else if (ndim == 2) {
-        fields1 <- cbind(fields1, fields2);
-      } else {
-        # This should never occur for a normal CDF structure.
-        fields1 <- append(fields1, fields2);
-      }
-      group1[[kk]] <- fields1;
-      rm(fields1, fields2);
+    group <- .subset2(groups, idx[1]);
+    nfields <- length(group);
+    if (nfields > 0) {
+      ffs <- 1:nfields;
+      idx <- idx[-1];
+      while(length(idx) > 0) {
+        groupNext <- .subset2(groups, idx[1]);
+  
+        # For each field...
+        for (ff in ffs) {
+          fields <- .subset2(group, ff);
+          fieldsNext <- .subset2(groupNext, ff);
+          ndim <- length(dim(fields));
+          if (ndim <= 1) {
+            fields <- append(fields, fieldsNext);
+          } else if (ndim == 2) {
+            fields <- cbind(fields, fieldsNext);
+          } else {
+            # This should never occur for a normal CDF structure.
+            fields <- append(fields, fieldsNext);
+          }
+          group[[ff]] <- fields;
+        }
+  
+        idx <- idx[-1];
+      } # while(...)
     }
-    res[[name]] <- group1;
-    rm(group1, group2, idx);
-  }
+
+    res[[name]] <- group;
+  } # for (name ...)
 
   res;
-} 
+} # cdfMergeStrands()
 
 
 ############################################################################
 # HISTORY:
+# 2008-02-22
+# o Updated so it now merges any unit with a even number of groups;
+#   a custom SNP CDF had three pairs of groups in part of their units.
 # 2006-03-07
 # o Renamed from cdfStandJoiner() to cdfMergeStrands().
 # 2006-02-23
