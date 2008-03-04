7e558f2f93615147d537611826bf81c81e0a1440
Martin Morgan <mtmorgan@fhcrc.org>
2008-03-04 20:16:00 +0000

    Version; 1.11.6 [2008-03-04] o Add ability to parse PGF and CLF files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@30361 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/readClfPgf.R
@@ -0,0 +1,29 @@
+readClf <- function(file, readBody=TRUE) {
+    ## FIXME: this is an exception in more recent TsvFile.cpp
+    if (!file.exists(file))
+        stop("clf file '", file, "' does not exist")
+    env <- new.env(parent=emptyenv())
+    .Call("R_affx_get_clf_file", file, readBody, env,
+          PACKAGE="affxparser")
+}
+
+readPgf <- function(file, readBody=TRUE) {
+    ## FIXME: this is an exception in more recent TsvFile.cpp
+    if (!file.exists(file))
+        stop("pgf file '", file, "' does not exist")
+    env <- new.env(parent=emptyenv())
+    .Call("R_affx_get_pgf_file", file, readBody, env,
+          PACKAGE="affxparser")
+}
+
+.pgfProbeIndexFromProbesetIndex <- function(pgf, probesetIdx) {
+    ## probeset -> atom
+    astarts <- pgf[["probesetStartAtom"]][probesetIdx]
+    aends <- pgf[["probesetStartAtom"]][probesetIdx+1]
+    aends[is.na(aends)] <- length(pgf[["atomId"]])
+    ## atom -> probe
+    pstarts <- pgf[["atomStartProbe"]][astarts]
+    pends <- pgf[["atomStartProbe"]][aends ] - 1
+    pends[is.na(aends)] <- length(pgf[["probeId"]])
+    mapply(seq, pstarts, pends, SIMPLIFY=FALSE)
+}
