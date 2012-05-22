cbd054c998cad0fd0ed0faffa6f748491d9cc6f4
Dan Tenenbaum <dtenenba@fhcrc.org>
2012-05-22 17:42:30 +0000

    don't assume R is in the path
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@66095 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testWriteAndReadEmptyCdf.R
+++ tests/testWriteAndReadEmptyCdf.R
@@ -1,4 +1,5 @@
-out <- system('R -e "affxparser:::.testWriteAndReadEmptyCdf()"', intern=TRUE);
+R_exe <- file.path(Sys.getenv("R_HOME"), "bin", "R")
+out <- system(paste(R_exe, '-e "affxparser:::.testWriteAndReadEmptyCdf()"'), intern=TRUE);
 cat(out, sep="\n");
 res <- any(regexpr("COMPLETE", out) != -1);
 cat("Test result: ", res, "\n", sep="");
