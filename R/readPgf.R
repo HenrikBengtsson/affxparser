da2d8d00fd29029936966a1ff65b3c703531af89
Henrik Bengtsson <hb@maths.lth.se>
2012-06-15 02:51:28 +0000

    Version: 1.29.4 [2012-06-14] o Updated the error messages for the CLF and PGF parsers.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@66743 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/readPgf.R
@@ -0,0 +1,59 @@
+readPgfHeader <- function(file) {
+  as.list(readPgfEnv(file, readBody=FALSE));
+}
+
+readPgf <- function(file, indices=NULL) {
+  as.list(readPgfEnv(file, readBody=TRUE, indices=indices));
+}
+
+readPgfEnv <- function(file, readBody=TRUE, indices=NULL) {
+  ## FIXME: this is an exception in more recent TsvFile.cpp
+  if (!file.exists(file)) {
+    stop("Cannot read PGF file. File not found: ", file); 
+  }
+  env <- new.env(parent=emptyenv());
+
+  if (!all(sort(indices) == indices)) {
+    stop("indices must be sorted.");
+  }
+    
+  res <- .Call("R_affx_get_pgf_file", file, readBody, env, indices,
+               PACKAGE="affxparser");
+
+  # Sanity check
+  if (is.null(res)) {
+    stop("Failed to read PGF file: ", file);
+  }
+
+  res;
+} # readPgfEnv()
+
+
+.pgfProbeIndexFromProbesetIndex <- function(pgf, probesetIdx) {
+  ## probeset -> atom
+  astarts <- pgf[["probesetStartAtom"]][probesetIdx];
+  aends <- pgf[["probesetStartAtom"]][probesetIdx+1];
+  aends[is.na(aends)] <- length(pgf[["atomId"]]) + 1;
+  ## atom -> probe
+  pstarts <- pgf[["atomStartProbe"]][astarts];
+  pends <- pgf[["atomStartProbe"]][aends] - 1;
+  pends[is.na(pends)] <- length(pgf[["probeId"]]);
+  mapply(":", pstarts, pends, SIMPLIFY=FALSE);
+} # .pgfProbeIndexFromProbesetIndex()
+
+
+############################################################################
+# HISTORY:
+# 2012-06-14 [HB]
+# o Moved all CLF functions to readClf.R.
+# o Harmonized the error messages.
+# 2011-11-18 [HB]
+# o ROBUSTNESS: Added sanity check that the native code did not return NULL.
+# 2008-03-24 [JB]
+# o ROBUSTNESS: Asserts that argument 'indices' to readClfEnv() and
+#   readPgfEnv() is ordered.
+# 2008-03-22 [JB]
+# o Added read(Clf|Pgf)Env() functions.
+# 2008-03-04 [MM]
+# o Added CLF and PGF parsers by Martin Morgan (MM).
+############################################################################
