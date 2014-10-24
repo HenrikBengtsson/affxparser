8c5f1053f0b1c86b8cb03c5947f7ea2d29586bf3
hb <hb@aroma-project.org>
2014-10-24 11:07:15 -0700

    Now readCelRectangle() gives an informative error message if argument 'xrange' or 'yrange' is not of length two.

--- tests/readCelRectangle.R
+++ tests/readCelRectangle.R
@@ -14,12 +14,19 @@ if (require("AffymetrixDataTestFiles")) {
   cel <- file.path(pathD, "2.Calvin", "HG-Focus-1-121502.CEL")
 
   # Read CEL intensities in the upper left corner
-  data <- readCelRectangle(cel, xrange=c(0,250), yrange=c(0,250))
+  range <- c(0,250)
+  data <- readCelRectangle(cel, xrange=range, yrange=range)
   z <- rotate270(data$intensities)
   sub <- sprintf("Chip type: %s", data$header$chiptype)
   image(z, col=gray.colors(256), axes=FALSE, main=basename(cel), sub=sub)
   text(x=0, y=1, labels="(0,0)", adj=c(0,-0.7), cex=0.8, xpd=TRUE)
   text(x=1, y=0, labels="(250,250)", adj=c(1,1.2), cex=0.8, xpd=TRUE)
+
+  # Read 1x1 rectangle
+  range <- c(0,0)
+  data <- readCelRectangle(cel, xrange=range, yrange=range)
+  print(data$intensities)
+  stopifnot(all(dim(data$intensities) == c(1,1)))
 }
 
 
