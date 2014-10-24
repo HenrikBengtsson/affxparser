f2fa38dcafdfd7ae1e7a59e5717f7edc7414c634
hb <hb@aroma-project.org>
2014-10-23 21:41:37 -0700

    # Added package test for readCdfDataFrame()

---
+++ tests/readCdfDataFrame.R
@@ -0,0 +1,34 @@
+if (require("AffymetrixDataTestFiles")) {
+  library("affxparser")
+
+  pathR <- system.file(package="AffymetrixDataTestFiles")
+  pathA <- file.path(pathR, "annotationData", "chipTypes", "Test3")
+
+  # Read CDF structure
+  cdf <- file.path(pathA, "1.XDA", "Test3.CDF")
+  hdr <- readCdfHeader(cdf)
+  Jall <- hdr$nunits
+
+  # Various sets of indices to be read
+  idxsList <- list(
+##  readNothing=integer(0L), # FIX ME
+    readAll=NULL,
+    readOne=10L,
+    readSome=11:20,
+    readDouble=as.double(11:20)
+  )
+
+  # Read full file
+  data <- readCdfDataFrame(cdf)
+  str(head(data))
+  J <- if (is.null(idxs)) Jall else length(idxs)
+  stopifnot(length(unique(data$unitName)) == Jall)
+
+  # Read different subsets of units
+  for (idxs in idxsList) {
+    data <- readCdfDataFrame(cdf, units=idxs)
+    str(head(data))
+    J <- if (is.null(idxs)) Jall else length(idxs)
+    stopifnot(length(unique(data$unitName)) == J)
+  }
+} # if (require("AffymetrixDataTestFiles"))
