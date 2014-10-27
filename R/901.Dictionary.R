a1d61ae7560d3d5837bb364f3b7963e6f6e8f0a8
hb <hb@aroma-project.org>
2014-10-26 22:16:14 -0700

    Pushing to BioC.

--- R/901.Dictionary.R
+++ R/901.Dictionary.R
@@ -6,7 +6,7 @@
 #
 #  \describe{
 #   \item{affxparser}{The name of this package.}
-#   \item{API}{Application program interface, which describes the 
+#   \item{API}{Application program interface, which describes the
 #     functional interface of underlying methods.}
 #   \item{block}{(aka group).}
 #   \item{BPMAP}{A file format containing information
@@ -17,24 +17,25 @@
 #   \item{cell}{(aka feature) A probe.}
 #   \item{cell index}{An integer that identifies a probe uniquely.}
 #   \item{chip}{An array.}
-#   \item{chip type}{An identifier specifying a chip design 
+#   \item{chip type}{An identifier specifying a chip design
 #     uniquely, e.g. \code{"Mapping50K_Xba240"}.}
 #   \item{DAT}{A file format: contains pixel intensity
 #      values collected from an Affymetrix GeneArray scanner.}
 #   \item{feature}{A probe.}
 #   \item{Fusion SDK}{Open-source sofware development kit (SDK) provided
 #     by Affymetrix to access their data files.}
-#   \item{group}{(aka block) 
-#     Defines a unique subset of the cells in a unit.  
+#   \item{group}{(aka block)
+#     Defines a unique subset of the cells in a unit.
 #     Expression arrays typically only have one group per unit, whereas
 #     SNP arrays have either two or four groups per unit, one for each of
 #     the two allele times possibly repeated for both strands.}
 #   \item{MM}{Mismatch-match, e.g. MM probe.}
-#   \item{TPMAP}{A file format storing the relationship between (PM,MM) 
+#   \item{PGF}{A file format: probe group file.}
+#   \item{TPMAP}{A file format storing the relationship between (PM,MM)
 #     pairs (or PM probes) and positions on a set of sequences.}
 #   \item{QC}{Quality control, e.g. QC probes and QC probe sets.}
 #   \item{unit}{A probeset.}
 #   \item{XDA}{A file format, aka as the binary file format.}
 #  }
 # }
-#*/#########################################################################  
+#*/#########################################################################
