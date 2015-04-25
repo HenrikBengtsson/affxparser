2bb26d3114f3e2c69c131c42805ac8f46c8b5938
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2015-04-25 22:55:31 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: c69edeb45d7cab94a2b5c86e55a43358a3811107
    
        Merge branch 'release/1.41.1'
    
    
    Commit id: f691ebfcfe9c78cb60e8336eff65d7f3894fa4bd
    
        affxparser 1.41.1 to Bioc devel
    
    
    Commit id: 6a99059afc1456c4e3437478000d24853ddb0e7e
    
        Travis updates
    
    
    Commit id: ef994f270d648df65f7798147b613792eb37d993
    
        MEMORY ERROR: Native R_affx_GetCHPGenotypingResults() had two non-PROTECT():ed usages of mkString(). [#14]
    
    
    Commit id: b383abe6c56aec4efffbbe635db723e3b0eecc6a
    
        Fixing two imbalanced PROTECT()/UNPROTECT() cases [#14]
    
    
    Commit id: 55c78f19b972ae3612fa69d5e7a90737ace5d023
    
        Merge branch 'master' into develop
    
    
    Commit id: bd04a618a90ce277002ad951bf6cbcb5e632156f
    
        Merge branch 'master' into develop
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affxparser@103087 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_affx_chp_parser.cpp
+++ src/R_affx_chp_parser.cpp
@@ -336,7 +336,7 @@ R_affx_GetCHPGenotypingResults(FusionCHPLegacyData *chp)
     PROTECT(ras1 = NEW_NUMERIC(qNbr));
     PROTECT(ras2 = NEW_NUMERIC(qNbr));
     nprotect+=2;
-    Rf_PrintValue(mkString("dudey"));
+    Rprintf("dudey");
   }
   else if(chp->GetHeader().GetAlgName() == L"DynamicModel") {
     bDynamicModel = true;
@@ -345,7 +345,7 @@ R_affx_GetCHPGenotypingResults(FusionCHPLegacyData *chp)
     PROTECT(bb = NEW_NUMERIC(qNbr));
     PROTECT(nocall = NEW_NUMERIC(qNbr));
     nprotect+=4;
-    Rf_PrintValue(mkString("howdy"));
+    Rprintf("howdy");
   }
 
 
@@ -559,7 +559,7 @@ R_affx_GetCHPEntries(FusionCHPQuantificationDetectionData *qData)
   SET_STRING_ELT(nms, 3, mkChar("ID"));
   SET_NAMES(result, nms);
 
-  UNPROTECT(5);
+  UNPROTECT(6);
   return result;
 }
 
@@ -765,7 +765,7 @@ R_affx_ReadCHP(FusionCHPQuantificationData *chp, bool isBrief)
   lstIdx = R_affx_AddCHPEntries(chp, lst, nms, lstIdx, isBrief);
 
   SET_NAMES(lst, nms);
-  UNPROTECT(2);
+  UNPROTECT(3);
   return(lst);
 }
 
