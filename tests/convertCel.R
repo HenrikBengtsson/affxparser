e168568e9c919a7c1bee7128f07e0247e92dd450
hb <hb@aroma-project.org>
2015-09-14 13:33:57 -0700

    TESTS: Add convertCel(..., newChipType) tests

--- tests/convertCel.R
+++ tests/convertCel.R
@@ -5,7 +5,7 @@ if (require("AffymetrixDataTestFiles")) {
   pathR <- system.file(package="AffymetrixDataTestFiles", mustWork=TRUE)
   pathR <- file.path(pathR, "rawData")
 
-  ## File #1: Test3
+  ## File #1a: Test3
   path <- file.path(pathR, "FusionSDK_Test3", "Test3", "2.Calvin")
   filename <- list.files(path=path, pattern="[.]CEL$")[1]
   pathname <- file.path(path, filename)
@@ -13,7 +13,15 @@ if (require("AffymetrixDataTestFiles")) {
   pathname4 <- convertCel(pathname, filename4)
   print(pathname4)
 
-  ## File #2: FusionSDK_HG-U133A
+  ## File #1b: Test3 with updated chip type
+  path <- file.path(pathR, "FusionSDK_Test3", "Test3", "2.Calvin")
+  filename <- list.files(path=path, pattern="[.]CEL$")[1]
+  pathname <- file.path(path, filename)
+  filename4 <- gsub(".CEL", ",v4,custom.CEL", filename)
+  pathname4 <- convertCel(pathname, filename4, newChipType="Test3-custom", verbose=TRUE)
+  print(pathname4)
+
+  ## File #2a: FusionSDK_HG-U133A
   path <- file.path(pathR, "FusionSDK_HG-U133A", "HG-U133A", "2.Calvin")
   filename <- list.files(path=path, pattern="[.]CEL$")[1]
   pathname <- file.path(path, filename)
@@ -21,11 +29,27 @@ if (require("AffymetrixDataTestFiles")) {
   pathname4 <- convertCel(pathname, filename4)
   print(pathname4)
 
-  ## File #3: FusionSDK_Focus
+  ## File #2b: FusionSDK_HG-U133A with new chip type
+  path <- file.path(pathR, "FusionSDK_HG-U133A", "HG-U133A", "2.Calvin")
+  filename <- list.files(path=path, pattern="[.]CEL$")[1]
+  pathname <- file.path(path, filename)
+  filename4 <- gsub(".CEL", ",v4,custom.CEL", filename)
+  pathname4 <- convertCel(pathname, filename4, newChipType="HG-U133A-custom", verbose=TRUE)
+  print(pathname4)
+
+  ## File #3b: FusionSDK_Focus
   path <- file.path(pathR, "FusionSDK_HG-Focus", "HG-Focus", "2.Calvin")
   filename <- list.files(path=path, pattern="[.]CEL$")[1]
   pathname <- file.path(path, filename)
   filename4 <- gsub(".CEL", ",v4.CEL", filename)
   pathname4 <- convertCel(pathname, filename4)
+  print(pathname4)
+
+  ## File #3b: FusionSDK_Focus
+  path <- file.path(pathR, "FusionSDK_HG-Focus", "HG-Focus", "2.Calvin")
+  filename <- list.files(path=path, pattern="[.]CEL$")[1]
+  pathname <- file.path(path, filename)
+  filename4 <- gsub(".CEL", ",v4,custom.CEL", filename)
+  pathname4 <- convertCel(pathname, filename4, newChipType="HG-U133A-custom", verbose=TRUE)
   print(pathname4)
 } # if (require("AffymetrixDataTestFiles"))
