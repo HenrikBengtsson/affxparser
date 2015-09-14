dde735575431ddfc5299f817cceb6912054b324c
hb <hb@aroma-project.org>
2015-09-14 13:57:37 -0700

    TESTS: More validation of convertCel() [#17]

--- tests/convertCel.R
+++ tests/convertCel.R
@@ -5,51 +5,79 @@ if (require("AffymetrixDataTestFiles")) {
   pathR <- system.file(package="AffymetrixDataTestFiles", mustWork=TRUE)
   pathR <- file.path(pathR, "rawData")
 
-  ## File #1a: Test3
-  path <- file.path(pathR, "FusionSDK_Test3", "Test3", "2.Calvin")
+  ## File #1: Test3
+  chipType <- "Test3"
+  path <- file.path(pathR, "FusionSDK_Test3", chipType, "2.Calvin")
   filename <- list.files(path=path, pattern="[.]CEL$")[1]
   pathname <- file.path(path, filename)
+  hdr <- readCelHeader(pathname)
+  str(hdr)
+  stopifnot(hdr$chiptype == chipType)
   filename4 <- gsub(".CEL", ",v4.CEL", filename)
   pathname4 <- convertCel(pathname, filename4, verbose=TRUE, .validate=TRUE)
   print(pathname4)
+  hdr4 <- readCelHeader(pathname4)
+  str(hdr4)
+  stopifnot(hdr4$chiptype == hdr$chiptype)
 
-  ## File #1b: Test3 with updated chip type
-  path <- file.path(pathR, "FusionSDK_Test3", "Test3", "2.Calvin")
-  filename <- list.files(path=path, pattern="[.]CEL$")[1]
-  pathname <- file.path(path, filename)
+  ## New chip type
+  newChipType <- sprintf("%s-custom", chipType)
   filename4 <- gsub(".CEL", ",v4,custom.CEL", filename)
-  pathname4 <- convertCel(pathname, filename4, newChipType="Test3-custom", verbose=TRUE)
+  pathname4 <- convertCel(pathname, filename4, newChipType=newChipType, verbose=TRUE)
   print(pathname4)
+  hdr4 <- readCelHeader(pathname4)
+  str(hdr4)
+## FIXME
+## stopifnot(hdr4$chiptype == newChipType)
 
-  ## File #2a: FusionSDK_HG-U133A
-  path <- file.path(pathR, "FusionSDK_HG-U133A", "HG-U133A", "2.Calvin")
+
+  ## File #2: FusionSDK_HG-U133A
+  chipType <- "HG-U133A"
+  path <- file.path(pathR, "FusionSDK_HG-U133A", chipType, "2.Calvin")
   filename <- list.files(path=path, pattern="[.]CEL$")[1]
   pathname <- file.path(path, filename)
+  hdr <- readCelHeader(pathname)
+  str(hdr)
+  stopifnot(hdr$chiptype == chipType)
   filename4 <- gsub(".CEL", ",v4.CEL", filename)
   pathname4 <- convertCel(pathname, filename4, verbose=TRUE, .validate=TRUE)
   print(pathname4)
+  hdr4 <- readCelHeader(pathname4)
+  str(hdr4)
+  stopifnot(hdr4$chiptype == hdr$chiptype)
 
-  ## File #2b: FusionSDK_HG-U133A with new chip type
-  path <- file.path(pathR, "FusionSDK_HG-U133A", "HG-U133A", "2.Calvin")
-  filename <- list.files(path=path, pattern="[.]CEL$")[1]
-  pathname <- file.path(path, filename)
+  ## New chip type
+  newChipType <- sprintf("%s-custom", chipType)
   filename4 <- gsub(".CEL", ",v4,custom.CEL", filename)
-  pathname4 <- convertCel(pathname, filename4, newChipType="HG-U133A-custom", verbose=TRUE)
+  pathname4 <- convertCel(pathname, filename4, newChipType=newChipType, verbose=TRUE)
   print(pathname4)
+  hdr4 <- readCelHeader(pathname4)
+  str(hdr4)
+  stopifnot(hdr4$chiptype == newChipType)
 
-  ## File #3b: FusionSDK_Focus
-  path <- file.path(pathR, "FusionSDK_HG-Focus", "HG-Focus", "2.Calvin")
+
+  ## File #3: FusionSDK_Focus
+  chipType <- "HG-Focus"
+  path <- file.path(pathR, "FusionSDK_HG-Focus", chipType, "2.Calvin")
   filename <- list.files(path=path, pattern="[.]CEL$")[1]
   pathname <- file.path(path, filename)
+  hdr <- readCelHeader(pathname)
+  str(hdr)
+  stopifnot(hdr$chiptype == chipType)
   filename4 <- gsub(".CEL", ",v4.CEL", filename)
   pathname4 <- convertCel(pathname, filename4, verbose=TRUE, .validate=TRUE)
   print(pathname4)
+  hdr4 <- readCelHeader(pathname4)
+  str(hdr4)
+  stopifnot(hdr4$chiptype == hdr$chiptype)
 
-  ## File #3b: FusionSDK_Focus
-  path <- file.path(pathR, "FusionSDK_HG-Focus", "HG-Focus", "2.Calvin")
-  filename <- list.files(path=path, pattern="[.]CEL$")[1]
-  pathname <- file.path(path, filename)
+  ## New chip type
+  newChipType <- sprintf("%s-custom", chipType)
   filename4 <- gsub(".CEL", ",v4,custom.CEL", filename)
-  pathname4 <- convertCel(pathname, filename4, newChipType="HG-U133A-custom", verbose=TRUE)
+  pathname4 <- convertCel(pathname, filename4, newChipType=newChipType, verbose=TRUE)
   print(pathname4)
+  hdr4 <- readCelHeader(pathname4)
+  str(hdr4)
+##  FIXME
+##  stopifnot(hdr4$chiptype == newChipType)
 } # if (require("AffymetrixDataTestFiles"))
